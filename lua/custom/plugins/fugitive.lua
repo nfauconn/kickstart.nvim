96ea4df25c33050aecbcd60c0e0b266538669e7a
Eden Fauconnier <eden.fauconnier@astran.ai>
2026-02-05 16:18:15 +0100

    fugitive

---
+++ lua/custom/plugins/fugitive.lua
@@ -0,0 +1,7 @@
+return {
+  'tpope/vim-fugitive',
+  keys = {
+    { '<leader>gs', '<cmd>Git<CR>', desc = 'Git status (Fugitive)' },
+    { '<space>gp', '<cmd>Git push<CR>', desc = 'Git push (Fugitive)' },
+  },
+}
